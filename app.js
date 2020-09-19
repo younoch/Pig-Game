0b1a1e0bdc70d66c914566d431620dcd83017eb8
younoch <younoch295@outlook.com>
2020-09-19 14:52:10 +0600

    19 sep

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ document.querySelector('.btn-hold').addEventListener('click',function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
             //Check if player won the game 
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 200) {
             document.querySelector('#name-'+ activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
